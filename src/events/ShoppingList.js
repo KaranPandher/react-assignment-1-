9bd5b6afcc0dfd34b1d0263f1b5052262e5d6121
Karan Pandher <karanpandher@live.ca>
2020-04-15 14:34:10 -0600

    Local Storage

--- src/events/ShoppingList.js
+++ src/events/ShoppingList.js
@@ -21,9 +21,33 @@ class ShoppingList extends Component {
     this.handleOnSubmit = this.handleOnSubmit.bind(this)
     this.validateFields = this.validateFields.bind(this)
   }
+  //Used for local storage and updates the local storage when any part of the stage is updated. Method provides acccess to the previous version of this state so we can make a comparision between the two version and determine if we want to perform this action 
+  componentDidUpdate(prevProps, prevState){
+    const prevStateString = JSON.stringify(prevState.groceryItems)
+    const updatedStateString = JSON.stringify(this.state.groceryItems)
+    // Pulls values from previous state and compares it to the update state. We are storing stringify versions. 
 
-  handleOnChange(e) {
-    const target = e.target
+    // This makes sure there is a difference between the two states and if there is we want to perform localstorage.setItem this updates the string and stores it in the browsers storage. 
+    // This only saves them but doesnt retrieve them. 
+    if (prevStateString !== updatedStateString) {
+      console.log("Save this:", updatedStateString)
+      localStorage.setItem('groceryItems', updatedStateString)
+    }
+  }
+  // This is used when the component is saved already (above) and now it will MOUNT it on the new reload and retrieve it from the local storage. 
+  componentDidMount() {
+    const savedStateFromLocalStorage = localStorage.getItem('groceryItems')
+    
+    if (savedStateFromLocalStorage) {
+      this.setState({
+        // We use JSON.parse so we can covnert it back to a string! 
+        groceryItems: JSON.parse(savedStateFromLocalStorage)
+      })
+    }
+  }
+
+  handleOnChange(event) {
+    const target = event.target
     const name = target.name
     const value = target.value
 
@@ -32,23 +56,31 @@ class ShoppingList extends Component {
     })
   }
 
-  handleCompletedToggle(e) {
-    const target = e.target
+  handleCompletedToggle(event) {
+    const target = event.target
     const itemindexValue = target.attributes.itemindex.value
     const index = parseInt(itemindexValue, 10) // access input's custom attribute
     
     console.log('toggling: ' + index)
-
+    
+    //creates brand new array
     const newGroceryItemsState = [...this.state.groceryItems]
-    newGroceryItemsState[index].completed = target.checked
+    //newGroceryItemsState[index].completed = target.checked
+    
+    // The lne above will be repalced. We create a brand new array the completed key is mutating this.state directly and we dont want that. So we will replace this code with a whole new object for the item being toggled and in this object the completed key gets the new value. 
+
+    newGroceryItemsState[index] = {
+      ...newGroceryItemsState[index],
+      completed: target.checked
+    }
 
     this.setState({
       groceryItems: newGroceryItemsState
     })
   }
 
-  handleDelete(e) {
-    const target = e.target
+  handleDelete(event) {
+    const target = event.target
     const itemindexValue = target.attributes.itemindex.value
     const index = parseInt(itemindexValue, 10) // access button's custom attribute
 
@@ -62,15 +94,18 @@ class ShoppingList extends Component {
     })
   }
 
-  handleOnSubmit(e) {
-    e.preventDefault()
+  handleOnSubmit(event) {
+    event.preventDefault()
 
     const isFormValid = this.validateFields()
 
     if (isFormValid) {
+       
       const newGroceryItemObject = {
         completed: false,
-        name: this.state.newItemName
+        name: this.state.newItemName,
+      // Have to add the ids from above . This ensures any new item that is made will have an id number now in this case item-xxx
+        id: 'item-' + Date.now()
       }
       this.setState((state) => {
         return {
