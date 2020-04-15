c1be71aec05a5d07883fb494b61fe13777dc6753
Karan Pandher <karanpandher@live.ca>
2020-04-14 23:36:41 -0600

    Shopping List and Logins

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
-//import './App.css';
+import './App.css';
 
-//import Card from './Card.js';
-//import './Card.css';
+import Card from './Card.js';
+import './Card.css';
 
 import InputEvents from './events/InputEvents'
 import MovementEvents from './events/MovementEvents'
 import './events/Events.css';
 
+import FormValidation from './events/FormValidation'
+
+import ShoppingList from './events/ShoppingList'
+
+
 function App() {
   return (
     <div className="App">
-      {/* <Card /> 
       <Card /> 
+      {/* <Card /> 
       <Card /> */} 
+      <FormValidation/>
       <InputEvents/>
       <MovementEvents/>
+      <ShoppingList/>
+      
     </div>
   );
 }
